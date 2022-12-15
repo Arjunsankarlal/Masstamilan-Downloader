74d793a3973d4e941954c7c207476ea78296b022
Arjun Sankarlal <arjun.sankarlal@paytm.com>
2022-12-15 22:19:24 +0530

    Kaayapatta singathoda moochu..

--- downloader/extract_zips.py
+++ downloader/extract_zips.py
@@ -8,8 +8,11 @@ destination_folder = "/path/to/extract/zips"
 for file in tqdm(os.listdir(path_to_zips_folder)):
     filename = path_to_zips_folder + "/" + file;
     if filename.__contains__(".zip"):
-        with zipfile.ZipFile(filename, 'r') as zip_ref:
-            folder_name = file.split("-320")[0]
-            zip_ref.extractall(destination_folder + "/" + folder_name)
+        try:
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                folder_name = file.split("-320")[0]
+                zip_ref.extractall(destination_folder + "/" + folder_name)
+        except Exception as e:
+            print(f"Exception while unzipping {filename}", e)
 print("Done and Dusted!!")
 print("Before deleting the zips make sure all the folders are extracted, once")
--- downloader/main.py
+++ downloader/main.py
@@ -1,32 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from datetime import datetime
+from time import perf_counter, sleep
 
 music_director_name = "a.r.rahman"
 download_directory = "path/to/your/desination"
 
 site_name = f"https://masstamilan.dev/music/{music_director_name}"
-pages = 200  # To accommodate all the films of the director
+pages = 50  # To accommodate all the films of the director
 
 chromeOptions = webdriver.ChromeOptions()
 
 prefs = {"download.default_directory": download_directory}
 chromeOptions.add_experimental_option("prefs", prefs)
-
 driver = webdriver.Chrome(options=chromeOptions)
 download_count = 1
-print(f'Start Time: {time.time()}')
+print(f'Start Time: {datetime.now()}')
+st = perf_counter()
 for page in range(1, pages):
-    site_page = site_name + f"?page={page}"
-    driver.get(site_page)
-    elements = driver.find_elements(By.CLASS_NAME, 'a-i')
-    for i in elements:
-        movie_link = i.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        driver.get(movie_link)
-        print(f"Page no is {i} and downloading {download_count} movie link is {movie_link}")
-        download_links = driver.find_element(By.CLASS_NAME, "dlink.anim")
-        driver.get(download_links.get_attribute("href"))
-        time.sleep(15)
-        driver.back()
-        download_count += 1
-print(f'End Time: {time.time()}')
+    try:
+        site_page = site_name + f"?page={page}"
+        driver.get(site_page)
+        elements = driver.find_elements(By.CLASS_NAME, 'a-i')
+        movie_link = ""
+        for i in elements:
+            movie_link = i.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            driver.get(movie_link)
+            print(f"Page no is {page} and downloading {download_count} movie link is {movie_link}")
+            download_links = driver.find_element(By.CLASS_NAME, "dlink.anim")
+            driver.get(download_links.get_attribute("href"))
+            sleep(15)
+            driver.back()
+            download_count += 1
+    except Exception as e:
+        print(
+            f"Exception while downloading for page no {page} and downloading {download_count} movie link is {movie_link}", e)
+print(f'End Time: {datetime.now()}')
+print(f"Total time taken is {perf_counter() - st} secs")
+print("Now it is time to unzip things!")
